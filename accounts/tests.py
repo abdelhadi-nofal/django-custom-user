da5d47434f73b38305a0ccac707786accdeb585d
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-07-14 19:22:28 +0300

    fin

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
+from django.contrib.auth import get_user_model
+
 # Create your tests here.
+
+
+
+class django_custom_test(TestCase):
+
+    def setUp(self):
+
+
+        self.user = get_user_model().objects.create_user(
+            username = 'hadi',
+            email = 'hadi19932@gmail.com',
+            password = 'hadi12345'
+        )
+
+    def test_create_new_user(self):
+
+        self.assertEquals(self.user.email,'hadi19932@gmail.com')
+        self.assertEquals(self.user.username,'hadi')
+        
+
+    def test_duplicate_emails(self):
+            pass
